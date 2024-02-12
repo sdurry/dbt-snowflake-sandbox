fcaf2d3e92db31adf81b1c7717aed732eb746cb7
Stephan Durry <stephan.durry@fishtownanalytics.com>
2024-02-12 09:26:12 +0000

    change col name

--- models/marts/core/fct_order_items.sql
+++ models/marts/core/fct_order_items.sql
@@ -43,7 +43,7 @@ final as (
         order_item.discounted_item_sales_amount,
         order_item.item_discount_amount,
         order_item.item_tax_amount,
-        order_item.net_item_sales_amount
+        order_item.net_item_sales_amount as net_item_sales_amount_in_eur
 
     from
         order_item
